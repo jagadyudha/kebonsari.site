7f5b7c4e25542e1ba7d1db573729bf4cc48adb04
unknown <jagadyudhaawali@gmail.com>
2022-02-17 23:32:18 +0700

    style: responsive on hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,25 +1,31 @@
 import React from "react";
 import Image from "next/image";
+import { AiOutlineArrowDown } from "react-icons/ai";
 
 const Hero = () => {
   return (
-    <div className="h-[92vh] bg-gray-200">
+    <div className="h-[92vh] bg-gray-200 flex xl:block justify-center">
       <div className="flex justify-between items-center">
-        <div className="mx-auto w-[50vw] pl-40 pr-40">
-          <h1 className="text-6xl font-bold text-gray-800">
-            Pemerintah Desa{" "}
-            <span className="text-sky-600 hover:underline transition-all duration-300">
-              KEBONSARI
-            </span>
+        <div className="mx-auto xl:w-[50vw] w-full xl:px-40 md:px-12 px-6">
+          <h1 className="md:text-6xl text-4xl font-bold text-gray-800">
+            Pemerintah Desa <span className="text-sky-600">KEBONSARI</span>
           </h1>
-          <p className="mt-4 text-xl text-gray-600">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+          <p className="my-4 md:text-xl text-md text-gray-600">
+            Programming today is a race between software engineers striving to
+            build bigger and better idiot-proof programs, and the Universe
+            trying to produce bigger and better idiots. So far, the Universe is
+            winning.
           </p>
+          <button>
+            <a href="#data">
+              <p className="mt-10 flex justify-center items-center px-8 py-4 rounded-md text-white bg-sky-600 font-medium hover:bg-sky-800 duration-300 transition-all shadow-sky-600">
+                <AiOutlineArrowDown className="mr-2 text-2xl animate-bounce" />
+                Jelajahi Sekarang
+              </p>
+            </a>
+          </button>
         </div>
-        <div className="w-[49.5vw] h-full">
+        <div className="w-[49.5vw] h-full xl:block hidden">
           <Image
             src={"/assets/images/hero.png"}
             width="100wh"
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { RiContactsLine } from "react-icons/ri";
 
 const Navbar = () => {
   return (
-    <nav className="h-[8vh] shadow-md px-20 sticky top-0 bg-white z-40">
-      <div className="flex items-center h-full">
+    <nav className="shadow-md xl:px-20 px-6 sticky top-0 bg-white z-40 flex justify-between py-5">
+      <div className="flex items-center w-1/3 xl:w-1/2">
         <img src="/assets/images/logo.png" />
       </div>
+      <button>
+        <p className="flex justify-center items-center border border-sky-600 md:px-4 px-2 md:py-2 py-1 rounded-md text-sky-600 font-medium hover:text-white hover:bg-sky-600 transition-all duration-300">
+          <RiContactsLine className="mr-2" />
+          Hubungi Kami
+        </p>
+      </button>
     </nav>
   );
 };
